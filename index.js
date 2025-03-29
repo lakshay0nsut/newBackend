08167f15900008e019f0fa735517c18e944b0d8d
Lakshay <lakshaysehrawat2111@gmail.com>
2025-03-29 23:09:47 +0530

    git commit -m "first commit"

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter',(req,res)=>{
+    res.send('MUH me lele')
+})
+
+app.get('/xnxx',(req,res)=>{
+    res.send('<h1>HARSH KI GAND FAAD</h1>')
+})
+app.get('/xhamster',(req,res)=>{
+    res.send("<h2>Noty hora ha ke BKL</h2>")
+})
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
