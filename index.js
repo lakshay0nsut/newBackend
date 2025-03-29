92da1580149d04280521a6616062c7beff9e1e52
Lakshay <lakshaysehrawat2111@gmail.com>
2025-03-29 23:21:23 +0530

    bakchodi

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ app.get('/xnxx',(req,res)=>{
 app.get('/xhamster',(req,res)=>{
     res.send("<h2>Noty hora ha ke BKL</h2>")
 })
+app.get('/noty',(req,res)=>{
+  res.send("<h2>teri daddi ka</h2>")
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
